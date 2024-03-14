59513e9e9cc8f013909d922c616a80042beb7b95
João Vitor <79696304+DARKnx@users.noreply.github.com>
2024-03-14 17:52:02 +0000

    ...

--- src/assets/config.js
+++ src/assets/config.js
@@ -1,4 +1,4 @@
 export default {
     pageTitle: "Inova Presente",
-    baseURL: true ? "http://localhost:4000/v1": "https://api.agenciamaisfacil.com.br:9000/v1" 
+    baseURL: false ? "http://localhost:4000/v1": "https://api.agenciamaisfacil.com.br:9000/v1" 
 }
